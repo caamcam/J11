946b62aa4cf72a8259b7f898f18f5e917518669a
caamcam <67265946+caamcam@users.noreply.github.com>
2020-07-09 16:50:23 +0200

    J11

--- EXO_02/suppr-produit.php
+++ EXO_02/suppr-produit.php
@@ -4,22 +4,20 @@ function connect_to_database()
     $servername ="localhost";
     $username = "root";
     $password = "";
-    $databasename = "ajout-produit";
+    $databasename = "testbase1";
+
 try 
 {
     $pdo = new PDO("musql :host=$servername;dbname=$databasename",$username,$password);
     $pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+  
     echo 'connecter successfully';
-    
     return $pdo;
     
 } 
- catch (PDOExeption $e)
- {
+ catch (PDOExeption $e){
      echo "connection failed : ". $e ->getMAssage();
     } 
 } 
-
-$sth = $dbh -> prepare('SELECT id=8 , noir, noire');
-
-?>
+?> 
